8e88612e6e744dd0c2e51bcfb364f3219ca1e902
Gabryel <gabryel329@gmail.com>
2023-05-24 10:24:45 -0300

    Ajustes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,15 +22,6 @@
       <div class="col-md-6 col-lg-3">
         <div class="widget-small primary coloured-icon"><i class="icon fa fa-users fa-3x"></i>
           <div class="info">
-            <h4>Usuarios</h4>
-
-                <p><b>{{$users}}</b></p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6 col-lg-3">
-        <div class="widget-small info coloured-icon"><i class="icon fa fa-thumbs-o-up fa-3x"></i>
-          <div class="info">
             <h4>Reembolsos</h4>
             @forelse ( $reembolsos as $reembolso )
                             <p><b>{{$reembolso->reembolsos}}</b></p>
@@ -41,13 +32,25 @@
         </div>
       </div>
       <div class="col-md-6 col-lg-3">
+        <div class="widget-small info coloured-icon"><i class="icon fa fa-thumbs-o-up fa-3x"></i>
+            <div class="info">
+                <h4>Reembolsos<br><span><p style="font-size: smaller;">(Glosados)</p></span></h4>
+                @forelse ( $emAbertos as $emAberto )
+                                <p><b>{{$emAberto->abertos}}</b></p>
+                            @empty
+                                <p><b>0 Em Abertos</b></p>
+                @endforelse
+              </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3">
         <div class="widget-small warning coloured-icon"><i class="icon fa fa-files-o fa-3x"></i>
           <div class="info">
             <h4>Reembolsos<br><span><p style="font-size: smaller;">(Em Abertos)</p></span></h4>
-            @forelse ( $emAbertos as $emAberto )
-                            <p><b>{{$emAberto->abertos}}</b></p>
+            @forelse ( $glosados as $glosado )
+                            <p><b>{{$glosado->glosado}}</b></p>
                         @empty
-                            <p><b>0 Em Abertos</b></p>
+                            <p><b>0 Glosados</b></p>
             @endforelse
           </div>
         </div>
