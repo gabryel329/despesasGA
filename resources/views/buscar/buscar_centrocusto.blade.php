81deb0d41edbcf57b4e58d83c5c1f9263bbb2309
gabryel329 <gabryel329@gmail.com>
2024-02-26 18:06:36 -0300

    add

--- resources/views/buscar/buscar_centrocusto.blade.php
+++ resources/views/buscar/buscar_centrocusto.blade.php
@@ -4,35 +4,35 @@
     <div class="app-content">
         <div class="app-title">
             <div>
-                <h1><i class="fa fa-edit"></i>Lista de Centro de Custo</h1>
+                <h1><i class="fa fa-edit"></i> Lista de Natureza Operação</h1>
             </div>
             <ul class="app-breadcrumb breadcrumb">
                 <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
                 <li class="breadcrumb-item">Administrativo</li>
-                <li class="breadcrumb-item"><a href="#">Centro Custo</a></li>
+                <li class="breadcrumb-item"><a href="#">Natureza Operação</a></li>
             </ul>
         </div>
         @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
-    @if(session('alert-warning'))
-        <div class="alert alert-warning">
-            {{ session('alert-warning') }}
-        </div>
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+        @if(session('alert-warning'))
+            <div class="alert alert-warning">
+                {{ session('alert-warning') }}
+            </div>
         @endif
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-12">
                 <div class="tile">
                     <div class="tile-title-w-btn">
                         <h3 class="title">Filtro</h3>
-                        <p><a class="btn btn-primary icon-btn" href="/centrocusto"><i class="fa fa-file"></i>Novo</a></p>
+                        <p><a class="btn btn-primary icon-btn" href="/gastos"><i class="fa fa-file"></i> Novo</a></p>
                     </div>
                     <div class="tile-body">
-                        <form action="{{ route('pesquisarcentrocusto.search') }}" method="post">
+                        <form action="{{ route('pesquisargastos.search') }}" method="post">
                             @csrf
-                            <input id="search" class="form-control input-md" type="text" name="search" placeholder="">
+                            <input id="search" class="form-control input-sm" type="text" name="search" placeholder="">
                         </form>
                     </div>
                 </div>
@@ -40,74 +40,59 @@
         </div>
         <div class="row">
             <div class="col-12">
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table id="tabela" class="table table-hover thead-light table table-striped table table-bordered">
-                        <thead class="text-primary" style="text-align: center;">
-                            <tr>
-                                <th>
-                                    Nome
-                                </th>
-                                <th>
-                                    Cidade
-                                </th>
-                                <th>
-                                    Estado
-                                </th>
-                                <th>
-                                    Observação
-                                </th>
-                                <th>
-                                    Excluir
-                                </th>
-                                <th>
-                                    Detalhes
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody style="text-align: center">
-                            @forelse($centrocustos as $centrocusto)
+                <div class="card-body">
+                    <div class="table-responsive-md">
+                        <table id="tabela" class="table table-hover thead-light table table-striped table table-bordered">
+                            <thead class="text-primary" style="text-align: center;">
                                 <tr>
-                                    <td>{{ $centrocusto->nome }}</td>
-                                    <td>{{ $centrocusto->cidade }}</td>
-                                    <td>{{ $centrocusto->estado }}</td>
-                                    <td>{{ $centrocusto->observacao }}</td>
-                                    <td>
-                                        <form action="{{ route('buscarcentrocusto.destroy', $centrocusto->id) }}" method="post"
-                                            class="ms-2">
-                                            @csrf
-                                            @method('DELETE')
-
-                                            <button class="btn btn-sm">
-                                                <i class="fa fa-trash center" style="color: red" aria-hidden="true"></i>
-                                            </button>
-
-                                        </form>
-
-                                    </td>
-                                    <td>
-                                        <div>
-                                            <button type="button" class="btn btn-sm">
-                                                <a href="{{ route('editarcentrocusto.edit', $centrocusto->id) }}"><i class="fa fa-eye" aria-hidden="true"></i></a>
-                                            </button>
-                                        </div>
-                                    </td>
+                                    <th>Nome</th>
+                                    <th>Observação</th>
+                                    <th>Excluir</th>
+                                    <th>Detalhes</th>
+                                </tr>
+                            </thead>
+                            <tbody style="text-align: center">
+                                @forelse($gastos as $gasto)
+                                    <tr>
+                                        <td>{{ $gasto->nome }}</td>
+                                        <td>{{ $gasto->observacao }}</td>
+                                        <td>
+                                            <form action="{{ route('buscargastos.destroy', $gasto->id) }}" method="post"
+                                                class="ms-2">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="btn btn-sm">
+                                                    <i class="fa fa-trash center" style="color: red" aria-hidden="true"></i>
+                                                </button>
+                                            </form>
+                                        </td>
+                                        <td>
+                                            <div>
+                                                <button type="button" class="btn btn-sm">
+                                                    <a href="{{ route('mostrargastos.show', $gasto->id) }}"><i class="fa fa-eye" aria-hidden="true"></i></a>
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr>
                                 @empty
-                                    <p class="alert-warning" style="font-size:22px;"center>Nenhum Centro Custo
-                                        Cadastrado
-                                    </p>
-                            @endforelse
-                            </tr>
-                        </tbody>
-                    </table>
+                                    <tr>
+                                        <td colspan="4">
+                                            <p class="alert-warning" style="font-size: 16px; text-align: center;">Nenhum Tipo de Gasto Cadastrado</p>
+                                        </td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     </div>
 @endsection
+
 @push('scripts')
 <script>
-    document.getElementById("menuclique").click()
+    document.getElementById("menuclique").click();
 </script>
 @endpush
+
--- resources/views/buscar/buscar_usuarios.blade.php
+++ resources/views/buscar/buscar_usuarios.blade.php
@@ -4,7 +4,7 @@
     <div class="app-content">
         <div class="app-title">
             <div>
-                <h1><i class="fa fa-edit"></i>Lista de Usuarios</h1>
+                <h1><i class="fa fa-edit"></i> Lista de Usuarios</h1>
             </div>
             <ul class="app-breadcrumb breadcrumb">
                 <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
@@ -13,95 +13,87 @@
             </ul>
         </div>
         @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
-    @if(session('alert-warning'))
-        <div class="alert alert-warning">
-            {{ session('alert-warning') }}
-        </div>
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+        @if(session('alert-warning'))
+            <div class="alert alert-warning">
+                {{ session('alert-warning') }}
+            </div>
         @endif
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-12">
                 <div class="tile">
                     <div class="tile-title-w-btn">
                         <h3 class="title">Filtro</h3>
-                        <p><a class="btn btn-primary icon-btn" href="/usuario"><i class="fa fa-file"></i>Novo</a></p>
+                        <p><a class="btn btn-primary icon-btn" href="/usuario"><i class="fa fa-file"></i> Novo</a></p>
                     </div>
                     <div class="tile-body">
                         <form action="{{ route('pesquisarusuarios.search') }}" method="post">
                             @csrf
-                            <input id="search" class="form-control input-md" type="text" name="search" placeholder="">
+                            <input id="search" class="form-control input-sm" type="text" name="search" placeholder="">
                         </form>
                     </div>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="card-body">
-                <div class="table-responsive-md">
-                    <table id="tabela" class="table table-hover thead-light table table-striped table table-bordered">
-                        <thead class="text-primary" style="text-align: center;">
-                            <tr>
-                                <th>
-                                    Nome
-                                </th>
-                                <th>
-                                    E-mail
-                                </th>
-                                <th>
-                                    Admin
-                                </th>
-                                <th>
-                                    Excluir
-                                </th>
-                                <th>
-                                    Detalhes
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody style="text-align: center">
-                            @forelse($usuarios as $usuario)
+            <div class="col-12">
+                <div class="card-body">
+                    <div class="table-responsive-md">
+                        <table id="tabela" class="table table-hover thead-light table table-striped table table-bordered">
+                            <thead class="text-primary" style="text-align: center;">
                                 <tr>
-                                    <td>{{ $usuario->name }}</td>
-                                    <td>{{ $usuario->email }}</td>
-                                    <td>{{ $usuario->admin }}</td>
-                                    <td>
-                                        <form action="{{ route('buscarusuarios.destroy', $usuario->id) }}" method="post"
-                                            class="ms-2">
-                                            @csrf
-                                            @method('DELETE')
-
-                                            <button class="btn btn-sm">
-                                                <i class="fa fa-trash center" style="color: red" aria-hidden="true"></i>
-                                            </button>
-
-                                        </form>
-
-                                    </td>
-                                    <td>
-                                        <div>
-                                            <button type="button" class="btn btn-sm">
-                                                <a href="{{ route('mostrarusuarios.show', $usuario->id) }}"><i class="fa fa-eye" aria-hidden="true"></i></a>
-                                            </button>
-                                        </div>
-                                    </td>
+                                    <th>Nome</th>
+                                    <th>E-mail</th>
+                                    <th>Admin</th>
+                                    <th>Excluir</th>
+                                    <th>Detalhes</th>
+                                </tr>
+                            </thead>
+                            <tbody style="text-align: center">
+                                @forelse($usuarios as $usuario)
+                                    <tr>
+                                        <td>{{ $usuario->name }}</td>
+                                        <td>{{ $usuario->email }}</td>
+                                        <td>{{ $usuario->admin }}</td>
+                                        <td>
+                                            <form action="{{ route('buscarusuarios.destroy', $usuario->id) }}" method="post"
+                                                class="ms-2">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="btn btn-sm">
+                                                    <i class="fa fa-trash center" style="color: red" aria-hidden="true"></i>
+                                                </button>
+                                            </form>
+                                        </td>
+                                        <td>
+                                            <div>
+                                                <button type="button" class="btn btn-sm">
+                                                    <a href="{{ route('mostrarusuarios.show', $usuario->id) }}"><i class="fa fa-eye" aria-hidden="true"></i></a>
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr>
                                 @empty
-                                    <p class="alert-warning" style="font-size:22px;"center>Nenhum Usuario
-                                        Cadastrado
-                                    </p>
-                            @endforelse
-                            </tr>
-                        </tbody>
-                    </table>
+                                    <tr>
+                                        <td colspan="5">
+                                            <p class="alert-warning" style="font-size: 16px; text-align: center;">Nenhum Usuario Cadastrado</p>
+                                        </td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 @endsection
+
 @push('scripts')
 <script>
-    document.getElementById("menuclique").click()
+    document.getElementById("menuclique").click();
 </script>
 @endpush
